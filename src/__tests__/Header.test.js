8232c3fb4abe7babfb599616b875728dffc96665
g_zawie <gzawie24@gmail.com>
2025-04-11 09:14:57 +0300

    testing jest

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
 
 import Header from "../components/Header";
 
